ac685a9d32a6d6e8f4d354637102c95b6af45f57
Darian Lagman <lagmand@myumanitoba.ca>
2025-03-21 01:42:31 -0500

    Fixed cli issue that couldnt see Logic package

--- UI/cli.py
+++ UI/cli.py
@@ -1,4 +1,10 @@
+import sys
+import os
 import click
+
+# Add the parent directory to sys.path so that Logic can be found
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from Logic.Simulation import run_simulation
 
 @click.command()
@@ -8,9 +14,9 @@ from Logic.Simulation import run_simulation
 @click.option('--theta', default=0.5, help='Opening angle parameter for the Barnes-Hut algorithm')
 @click.option('--bound-condition', default=10, help='Limit at which stars are no longer a part of the cluster')
 
-def cli(n, R0, timespan, theta, bound_condition):
+def cli(n, r0, timespan, theta, bound_condition):
     """CLI for running the N-body simulation."""
-    run_simulation(n, R0, timespan, theta, bound_condition)
+    run_simulation(n, r0, timespan, theta, bound_condition)
 
 if __name__ == '__main__':
     cli()
